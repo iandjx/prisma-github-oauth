9da4e3cc61b0b8200c2e9bc4f7bb76a80ece4886
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2020-03-17 06:36:29 +0900

    Added core-js, runtime

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
-import '@babel/polyfill/noConflict'
-import server from './server'
+import "@babel/polyfill/noConflict";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import server from "./server.js";
 
 server.start({ port: process.env.PORT || 4000 }, () => {
-    console.log('The server is up!')
-})
+  console.log("The server is up!");
+});
