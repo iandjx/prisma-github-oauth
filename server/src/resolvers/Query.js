ad47cb3218e26c29a60c36e6632197b65716cd9f
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2020-03-20 07:15:18 +0900

    Fixed header parameters

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,33 +1,42 @@
 import getUserId from "../utils/getUserId";
 
 const Query = {
-  users(parent, args, { prisma }, info) {
-    const opArgs = {
-      first: args.first,
-      skip: args.skip,
-      after: args.after,
-      orderBy: args.orderBy
-    };
+  // users(parent, args, { prisma }, info) {
+  //   const opArgs = {
+  //     first: args.first,
+  //     skip: args.skip,
+  //     after: args.after,
+  //     orderBy: args.orderBy
+  //   };
 
-    if (args.query) {
-      opArgs.where = {
-        OR: [
-          {
-            name_contains: args.query
-          }
-        ]
-      };
-    }
+  //   if (args.query) {
+  //     opArgs.where = {
+  //       OR: [
+  //         {
+  //           name_contains: args.query
+  //         }
+  //       ]
+  //     };
+  //   }
 
-    return prisma.query.users(opArgs, info);
-  },
+  //   return prisma.query.users(opArgs, info);
+  // },
 
-  me(parent, args, { prisma, request }, info) {
-    const userId = getUserId(request);
+  // me(parent, args, { prisma, request }, info) {
+  //   const userId = getUserId(request);
+
+  //   return prisma.query.user({
+  //     where: {
+  //       id: userId
+  //     }
+  //   });
+  // },
+  me: (parent, args, { prisma, request }) => {
+    const githubToken = getUserId(request);
 
     return prisma.query.user({
       where: {
-        id: userId
+        githubToken: githubToken
       }
     });
   },
